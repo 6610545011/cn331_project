a2b53903e99a276d75ecad847bee3d7607fcc9f5
6610545011 <142779636+6610545011@users.noreply.github.com>
2025-11-02 10:04:50 +0000

    update build.sh, settings.py and requirements

--- build.sh
+++ build.sh
@@ -4,6 +4,12 @@ set -o errexit
 
 pip install --upgrade pip
 pip install -r requirements.txt
+
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser --username admin --email "admin@email.com" --noinput || true
+
+# Create superuser automatically if not exists
+python manage.py createsuperuser \
+  --username admin \
+  --email "admin@email.com" \
+  --noinput || true
