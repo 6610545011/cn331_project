68ba9587b953b2308542630bedcf29021b993d0f
6610545011 <142779636+6610545011@users.noreply.github.com>
2025-11-14 20:14:07 +0000

    revert conflict between resolve conflict 2 and upvote downvote commit

--- review/urls.py
+++ review/urls.py
@@ -7,12 +7,11 @@ app_name = 'review'
 
 urlpatterns = [
     path('write/', views.write_review, name='write_review'),
-    path('vote/<int:review_id>/', views.vote_review, name='vote_review'),
-    path('bookmark/toggle/<int:review_id>/', views.toggle_bookmark, name='toggle_bookmark'),
-    path('report/<int:review_id>/', views.report_review, name='report_review'),
-
+    
     # AJAX URLs
     path('ajax/search-courses/', views.ajax_search_courses, name='ajax_search_courses'),
     path('ajax/get-professors/', views.ajax_get_professors, name='ajax_get_professors'),
-    path('ajax/get-sections/', views.ajax_get_sections_for_course, name='ajax_get_sections_for_course'),
+    path('ajax/get-sections/', views.ajax_get_sections, name='ajax_get_sections'),
+    path('bookmark/<int:review_id>/', views.toggle_bookmark, name='toggle_bookmark'),
+    path('report/<int:review_id>/', views.report_review, name='report_review'),
 ]
