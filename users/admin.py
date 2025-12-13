d8379f4ccb70032c505010b0705e1b07bbdd071e
6610545011 <142779636+6610545011@users.noreply.github.com>
2025-12-13 18:36:14 +0000

    add email box in admin page for create user

--- users/admin.py
+++ users/admin.py
@@ -1,14 +1,25 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    
+    # --- แก้ไขตรงนี้: เปลี่ยนเป็น password1 และ password2 ---
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2'),
+        }),
+    )
+
     fieldsets = UserAdmin.fieldsets + (
-        ('Extra Profile Info', {'fields': ('imgurl',)}),
+        ('Custom Info', {'fields': ('imgurl',)}),
     )
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        ('Extra Profile Info', {'fields': ('imgurl',)}),
-    )
+
     list_display = ('username', 'email', 'is_staff', 'is_active')
     search_fields = ('username', 'email')
     ordering = ('username',)
